89adc52c090b699e5a9ee1efe85ce0fd22704896
stefannaumov <stefannaumow@gmail.com>
2014-11-01 18:04:54 -0700

    added user login

--- ChatApp/ChatApp/MainViewController.m
+++ ChatApp/ChatApp/MainViewController.m
@@ -12,11 +12,14 @@
 
 @end
 
-@implementation MainViewController
+@implementation MainViewController{
+    PFUser *user;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
 	// Do any additional setup after loading the view, typically from a nib.
     
     //PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
@@ -31,5 +34,32 @@
 }
 
 - (IBAction)logInTouchUp:(id)sender {
+    user = [PFUser user];
+    NSString *username = self.userNameInput.text;
+    NSString *password = self.passwordInput.text;
+    
+    //hide keyboard
+    [self.userNameInput resignFirstResponder];
+    [self.passwordInput resignFirstResponder];
+    
+    [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser *user, NSError *error) {
+        UIAlertView *loginAlertView;
+        if (!error) {
+            loginAlertView = [[UIAlertView alloc] initWithTitle:@"Success" message:@"Logged in!" delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+            [loginAlertView show];
+        }
+        else{
+            loginAlertView = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Login failed!" delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+            [loginAlertView show];
+        }
+    }];
+    
+    //clear inputs
+    self.userNameInput.text = @"";
+    self.passwordInput.text = @"";
+}
+
+-(void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    [self.view endEditing:TRUE];
 }
 @end
